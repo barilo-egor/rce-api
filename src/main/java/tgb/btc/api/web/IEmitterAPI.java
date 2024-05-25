d190bc904fdb62a690c568bd18ca417e29baafab
barilo <barilo>
2024-05-26 01:38:45 +0300

    remove emitter api

--- src/main/java/tgb/btc/api/web/IEmitterAPI.java
+++
@@ -1,8 +0,0 @@
-package tgb.btc.api.web;
-
-import tgb.btc.api.web.constants.EmitterMessageType;
-
-public interface IEmitterAPI {
-
-    void message(EmitterMessageType type);
-}
--- src/main/java/tgb/btc/api/web/constants/EmitterMessageType.java
+++
@@ -1,7 +0,0 @@
-package tgb.btc.api.web.constants;
-
-public enum EmitterMessageType {
-    NEW_BOT_DEAL,
-    NEW_ADDITIONAL_VERIFICATION
-    ;
-}

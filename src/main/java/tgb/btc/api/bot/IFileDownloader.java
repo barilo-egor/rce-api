653eebd963794b5b3cbedd7c15a40dfdfc85aee7
barilo <e.barilo@isida.by>
2024-07-30 17:28:01 +0300

    saveFile

--- src/main/java/tgb/btc/api/bot/IFileDownloader.java
+++ src/main/java/tgb/btc/api/bot/IFileDownloader.java
@@ -1,6 +1,10 @@
 package tgb.btc.api.bot;
 
+import java.io.File;
+
 public interface IFileDownloader {
 
     void downloadFile(String imageId, String localPath);
+
+    String saveFile(File file);
 }
